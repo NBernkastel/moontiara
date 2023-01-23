200d39260a10b7c2746c5f68e92fb2671a9457ec
NBernkastel <nexeland@gmail.com>
2023-01-23 17:00:39 +0600

    22.01(3)
    Signed-off-by: dimpr <sensuse47@gmail.com>

--- moontiara/tiara/static/scripts/home_script.js
+++ moontiara/tiara/static/scripts/home_script.js
@@ -1,12 +1,26 @@
 let prog = document.querySelector(".progress-bar")
 let mn = document.querySelector(".main")
 let start = Date.now()
+prog.style.visibility = 'hidden'
 let timer = setInterval(function() {
 
     let timePassed = Date.now() - start;
     if (timePassed >= 3500) {
         prog.style.visibility = 'hidden'
-        mn.style.opacity = (timePassed-3500) / 50 + '%'
+        mn.style.opacity = (timePassed-3500) / 10 + '%'
+    }
+    if (timePassed >= 7000){
+        clearInterval(timer)
+        return
     }
 },20)
-
+let i = 0
+let card = document.querySelector(".card")
+let timer2 = setInterval(function() {
+    if (i > 100){
+        clearInterval(timer2)
+        return
+    }
+    card.style.left = i + '%'
+    i += 0.005
+},1)
